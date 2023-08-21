f3e4612112e32e9644f92ee5d51101aaa657b742
Madan Pattanshetty <madanpattanashetty4@gmail.com>
2023-08-21 12:41:02 +0530

    Updated app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+let button = document.getElementById("button");
+var numberInTheShape = 1;
+var OldValue = 0;
+
+button.onclick = () =>{
+    let n = document.getElementById("number").value;
+    const circle = document.getElementById("circle");
+    const square = document.getElementById("square");
+    const rect = document.getElementById("rectangle");
+    const $box = document.getElementById("box");
+    let j;
+
+    n = Number(OldValue) + Number(n);
+
+    for(let j = numberInTheShape; j<=n; j++){
+        var shape = document.createElement("div");
+        shape.innerHTML += numberInTheShape;
+        if(square.checked){
+            shape.classList.add("square");
+        }
+        else if(circle.checked){
+            shape.classList.add("circle");
+        }
+        else if(rect.checked){
+            shape.classList.add("rectangle");
+        }
+        else{
+            document.write("Invalid Input");
+        }
+        $box.appendChild(shape);
+        numberInTheShape++;
+        OldValue = document.getElementById("box").lastElementChild.innerHTML;
+    }
+};
